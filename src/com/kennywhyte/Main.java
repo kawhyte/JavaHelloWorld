6bc30a994fd5654940941f7e7bd6375360cd9542
Kenny <kawhyte@gmail.com>
2019-10-31 10:00:14 -0700

    Initial commit

---
+++ src/com/kennywhyte/Main.java
@@ -0,0 +1,18 @@
+package com.kennywhyte;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        short x = 1;
+        int y = x + 2;
+
+
+        System.out.println(y);
+
+
+    }
+}
