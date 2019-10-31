bd9ae369d46b719a6dad7377b27f3645544e69d5
Kenny <kawhyte@gmail.com>
2019-10-31 10:30:22 -0700

    Testing the Math Class

--- src/com/kennywhyte/Main.java
+++ src/com/kennywhyte/Main.java
@@ -7,12 +7,9 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        short x = 1;
-        int y = x + 2;
-
-
-        System.out.println(y);
-
-
+        Math.round(3.14);
+        int result = Math.max(12, 10);
+        double rand = (int) (Math.random() * 200);
+        System.out.println(rand);
     }
 }
