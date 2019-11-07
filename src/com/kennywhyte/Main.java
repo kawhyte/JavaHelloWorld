33ef2d8b62a5b3f0d8bfa663e757e5e2a9d4da0e
Kenny <kawhyte@gmail.com>
2019-11-06 21:07:20 -0800

    Classes and Objects

--- src/com/kennywhyte/Main.java
+++ src/com/kennywhyte/Main.java
@@ -13,40 +13,7 @@ public class Main {
         String message = greetUser("Kenny", "Whyte");
         System.out.println(message);
 
-        /*Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your Principle: ");
-        double principal = scanner.nextDouble();
-
-        System.out.print("Enter your Annual Interest Rate: ");
-        double rate = scanner.nextDouble();
-
-        System.out.print("Period:(Years): ");
-        int period = scanner.nextInt();
-
-        double interestRate = rate / 100 / 12;
-
-        double payment = (principal * interestRate) / (1 - Math.pow(1 + interestRate, -period));
-
-        double mortgage = (double) Math.round(payment * 100) / 100;
-
-        System.out.println("Mortgage " + mortgage);*/
-
-
-//        String[] fruits = {"apple", "banana", "Chicken"};
-//
-//
-//        System.out.println("Outside the loop: " + Arrays.toString(fruits));
-//
-//
-//        for (int i = 0; i < fruits.length; i++) {
-//            System.out.println(fruits[i]);
-//        }
-//
-//        for (String fruit : fruits
-//        ) {
-//            System.out.println("Last: " + fruit);
-//        }
 
     }
 
---
+++ src/com/kennywhyte/TextBox.java
@@ -0,0 +1,18 @@
+package com.kennywhyte;
+
+public class TextBox {
+
+    public String text;
+
+    public void setText(String text) {
+
+        //text =text;
+        this.text = text;
+
+    }
+
+    public void clear() {
+        this.text = "";
+
+    }
+}
