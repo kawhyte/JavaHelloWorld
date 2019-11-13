73f51991662e4cb85896dba4e1b67a5e4f82dfb9
Kenny <kawhyte@gmail.com>
2019-11-13 12:01:33 -0800

    Setter and Getters

--- src/com/kennywhyte/Employee.java
+++ src/com/kennywhyte/Employee.java
@@ -2,12 +2,30 @@ package com.kennywhyte;
 
 public class Employee {
 
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int _hourlyRate;
     //public int extraHours;
 
-    public int calculateWage(int extraHours) {
 
-        return baseSalary + (hourlyRate * extraHours);
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) throw new IllegalArgumentException("Salary cannot be zero");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void set_hourlyRate(int _hourlyRate) {
+
+        this._hourlyRate = _hourlyRate;
+    }
+
+    public int get_hourlyRate() {
+        return _hourlyRate;
+    }
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (_hourlyRate * extraHours);
     }
 }
--- src/com/kennywhyte/Main.java
+++ src/com/kennywhyte/Main.java
@@ -1,17 +1,13 @@
 package com.kennywhyte;
 
-import java.awt.*;
-import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+
         var employee = new Employee();
-        employee.baseSalary = 50000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50);
+        employee.set_hourlyRate(20);
+
         int wage = employee.calculateWage(10);
 
         System.out.println(wage);
