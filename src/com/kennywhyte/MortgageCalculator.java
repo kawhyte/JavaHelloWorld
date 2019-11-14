a318e46b3ae2e8244478036674030ae2178fc294
Kenny <kawhyte@gmail.com>
2019-11-14 15:51:36 -0800

    Refactored  - Mortgage Calculations (Remaining balance)

--- src/com/kennywhyte/MortgageCalculator.java
+++ src/com/kennywhyte/MortgageCalculator.java
@@ -2,6 +2,9 @@ package com.kennywhyte;
 
 public class MortgageCalculator {
 
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
+
     private int principal;
     private float annualInterest;
     private byte years;
@@ -14,8 +17,8 @@ public class MortgageCalculator {
 
 
     public double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -26,8 +29,8 @@ public class MortgageCalculator {
 
     public double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
@@ -36,7 +39,22 @@ public class MortgageCalculator {
         return mortgage;
     }
 
-    public short getYears() {
-        return years;
+    public double[] getRemainingBalances() {
+        double[] balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= getNumberOfPayments(); month++) {
+            double balance = calculateBalance(month);
+            balances[month - 1] = balance;
+            //System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+        return balances;
     }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
+    }
+
 }
